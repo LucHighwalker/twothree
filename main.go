bcf9508b3d8b4be99c829e84841bffab1ed407b3
LucHighwalker <luc.highwalker@gmail.com>
2019-12-02 13:08:06 -0800

    Added structs

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type Tree struct {
+	root *Node
+}
+type Node struct {
+	data     [2]*int
+	children [3]*Node
+	parent   *Node
+	tree     *Tree
+}
+
 func main() {
 	fmt.Println("Hello froma two three world")
 }
